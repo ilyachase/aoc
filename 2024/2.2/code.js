b2cf2135714c0ad82d990b176dbe13d88cf0e321
Ilya Levin <ilya.chase@yandex.ru>
2024-12-02 18:10:58 +0500

    2024.2

---
+++ 2024/2.2/code.js
@@ -0,0 +1,56 @@
+const fs = require('fs')
+
+function checkLevels(levels) {
+    const increasing = levels[1] > levels[0]
+
+    let isLevelSafe = true
+    levels.forEach((value, index, array) => {
+        if (index === 0 || !isLevelSafe) {
+            return
+        }
+
+        const levelsDifference = Math.abs(value - array[index - 1])
+        if (levelsDifference < 1 || levelsDifference > 3) {
+            isLevelSafe = false
+            return
+        }
+
+        if (increasing && value < array[index - 1]) {
+            isLevelSafe = false
+            return
+        }
+
+        if (!increasing && value > array[index - 1]) {
+            isLevelSafe = false
+        }
+    })
+
+    return isLevelSafe
+}
+
+const reports = fs.readFileSync('input.txt').toString().split('\n')
+
+let safeReportsCount = 0
+for (let report of reports) {
+    const levels = report.split(' ').map(str => parseInt(str))
+
+    let isLevelSafe = checkLevels(levels)
+    for (let index = 0; index < levels.length; index++) {
+        if (isLevelSafe) {
+            break
+        }
+
+        const splicedLevels = Array.from(levels)
+        splicedLevels.splice(index, 1)
+
+        isLevelSafe = checkLevels(splicedLevels)
+    }
+
+    if (isLevelSafe) {
+        safeReportsCount++
+    } else {
+        const c = 123
+    }
+}
+
+console.log(`Safe reports count: ${safeReportsCount}`)
