0a8c2f533c7ce6cb2c43ddc89d2d08ba715346fc
Ilya Levin <ilya.chase@yandex.ru>
2024-12-04 12:28:05 +0500

    4.1

--- 2024/4.1/code.js
+++ 2024/4.1/code.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 
 const words = fs.readFileSync('input.txt').toString().split('\n')
+// const words = fs.readFileSync('input_example.txt').toString().split('\n')
 
 function safeAccess(array, y, x) {
     if (array[y] && array[y][x] !== undefined) {
@@ -19,7 +20,7 @@ for (let y = 0; y < words.length; y++) {
         }
 
         // horizontal - backwards
-        if (safeAccess(words, y, x + 3) + safeAccess(words, y, x + 2) + safeAccess(words, y, x + 1) + safeAccess(words, y, x) === 'XMAS') {
+        if (safeAccess(words, y, x) + safeAccess(words, y, x - 1) + safeAccess(words, y, x - 2) + safeAccess(words, y, x - 3) === 'XMAS') {
             xmas++
         }
 
@@ -29,7 +30,7 @@ for (let y = 0; y < words.length; y++) {
         }
 
         // vertical - backwards
-        if (safeAccess(words, y + 3, x) + safeAccess(words, y + 2, x) + safeAccess(words, y + 1, x) + safeAccess(words, y, x) === 'XMAS') {
+        if (safeAccess(words, y, x) + safeAccess(words, y - 1, x) + safeAccess(words, y - 2, x) + safeAccess(words, y - 3, x) === 'XMAS') {
             xmas++
         }
 
@@ -39,17 +40,17 @@ for (let y = 0; y < words.length; y++) {
         }
 
         // diagonal - 2
-        if (safeAccess(words, y, x) + safeAccess(words, y - 1, x - 1) + safeAccess(words, y - 2, x - 2) + safeAccess(words, y - 3, x - 3) === 'XMAS') {
+        if (safeAccess(words, y, x) + safeAccess(words, y + 1, x - 1) + safeAccess(words, y + 2, x - 2) + safeAccess(words, y + 3, x - 3) === 'XMAS') {
             xmas++
         }
 
         // diagonal - backwards
-        if (safeAccess(words, y + 3, x + 3) + safeAccess(words, y + 2, x + 2) + safeAccess(words, y + 1, x + 1) + safeAccess(words, y, x) === 'XMAS') {
+        if (safeAccess(words, y, x) + safeAccess(words, y - 1, x + 1) + safeAccess(words, y - 2, x + 2) + safeAccess(words, y - 3, x + 3) === 'XMAS') {
             xmas++
         }
 
         // diagonal - 2 - backwards
-        if (safeAccess(words, y - 3, x - 3) + safeAccess(words, y - 2, x - 2) + safeAccess(words, y - 1, x - 1) + safeAccess(words, y, x) === 'XMAS') {
+        if (safeAccess(words, y, x) + safeAccess(words, y - 1, x - 1) + safeAccess(words, y - 2, x - 2) + safeAccess(words, y - 3, x - 3) === 'XMAS') {
             xmas++
         }
     }
