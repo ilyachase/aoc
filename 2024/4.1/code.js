d9935e3087c68f0e179fa06dace62bbad1f88c94
Ilya Levin <ilya.chase@yandex.ru>
2024-12-04 12:20:34 +0500

    4.1 attempt #1

---
+++ 2024/4.1/code.js
@@ -0,0 +1,58 @@
+const fs = require('fs')
+
+const words = fs.readFileSync('input.txt').toString().split('\n')
+
+function safeAccess(array, y, x) {
+    if (array[y] && array[y][x] !== undefined) {
+        return array[y][x];
+    }
+
+    return '';
+}
+
+let xmas = 0
+for (let y = 0; y < words.length; y++) {
+    for (let x = 0; x < words[0].length; x++) {
+        // horizontal
+        if (safeAccess(words, y, x) + safeAccess(words, y, x + 1) + safeAccess(words, y, x + 2) + safeAccess(words, y, x + 3) === 'XMAS') {
+            xmas++
+        }
+
+        // horizontal - backwards
+        if (safeAccess(words, y, x + 3) + safeAccess(words, y, x + 2) + safeAccess(words, y, x + 1) + safeAccess(words, y, x) === 'XMAS') {
+            xmas++
+        }
+
+        // vertical
+        if (safeAccess(words, y, x) + safeAccess(words, y + 1, x) + safeAccess(words, y + 2, x) + safeAccess(words, y + 3, x) === 'XMAS') {
+            xmas++
+        }
+
+        // vertical - backwards
+        if (safeAccess(words, y + 3, x) + safeAccess(words, y + 2, x) + safeAccess(words, y + 1, x) + safeAccess(words, y, x) === 'XMAS') {
+            xmas++
+        }
+
+        // diagonal
+        if (safeAccess(words, y, x) + safeAccess(words, y + 1, x + 1) + safeAccess(words, y + 2, x + 2) + safeAccess(words, y + 3, x + 3) === 'XMAS') {
+            xmas++
+        }
+
+        // diagonal - 2
+        if (safeAccess(words, y, x) + safeAccess(words, y - 1, x - 1) + safeAccess(words, y - 2, x - 2) + safeAccess(words, y - 3, x - 3) === 'XMAS') {
+            xmas++
+        }
+
+        // diagonal - backwards
+        if (safeAccess(words, y + 3, x + 3) + safeAccess(words, y + 2, x + 2) + safeAccess(words, y + 1, x + 1) + safeAccess(words, y, x) === 'XMAS') {
+            xmas++
+        }
+
+        // diagonal - 2 - backwards
+        if (safeAccess(words, y - 3, x - 3) + safeAccess(words, y - 2, x - 2) + safeAccess(words, y - 1, x - 1) + safeAccess(words, y, x) === 'XMAS') {
+            xmas++
+        }
+    }
+}
+
+console.log(xmas)
