564f9529da14245b0e189b238f443a89089ff923
Ilya Levin <ilya.chase@yandex.ru>
2024-12-11 21:39:58 +0500

    11.1

---
+++ 2024/11.1/code.js
@@ -0,0 +1,20 @@
+const fs = require('fs')
+
+const stones = fs.readFileSync('input.txt').toString().split(' ').map(v => parseInt(v))
+
+for (let i = 0; i < 75; i++) {
+    for (let j = 0; j < stones.length; j++) {
+        if (stones[j] === 0) {
+            stones[j] = 1
+        } else if (stones[j].toString().length % 2 === 0) {
+            const oldStone = stones[j].toString()
+            stones[j] = parseInt(oldStone.substring(0, Math.floor(oldStone.length / 2)))
+            stones.splice(j+1, 0, parseInt(oldStone.substring(Math.floor(oldStone.length / 2))))
+            j++
+        } else {
+            stones[j] *= 2024
+        }
+    }
+}
+
+console.log(stones.length)
