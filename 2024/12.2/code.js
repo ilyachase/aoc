2f227fd355b82468dfdc62250e2329fd48cc62fc
Ilya Levin <ilya.chase@yandex.ru>
2024-12-12 18:52:28 +0500

    12.2

--- 2024/12.2/code.js
+++ 2024/12.2/code.js
@@ -17,16 +17,27 @@ function buildRegion(map, plant, y, x, regionData, currentRegionPlots = []) {
         return
     }
 
-    if (currentRegionPlots.includes(y+','+x)) {
+    if (currentRegionPlots.includes(y + ',' + x)) {
         return
     }
 
     // update region area
     regionData.area++
-    // update region perimeter
 
-    currentRegionPlots.push(y+','+x)
-    observedPlots.push(y+','+x)
+    // update region perimeter
+    for (let coords of [[y + 1, x], [y, x + 1], [y - 1, x], [y, x - 1]]) {
+        let [y, x] = coords
+        if (y < 0 || y > map.length - 1) {
+            regionData.perimeter++
+        } else if (x < 0 || x > map[0].length - 1) {
+            regionData.perimeter++
+        } else if (map[y][x] !== plant) {
+            regionData.perimeter++
+        }
+    }
+
+    currentRegionPlots.push(y + ',' + x)
+    observedPlots.push(y + ',' + x)
 
     buildRegion(map, plant, y + 1, x, regionData, currentRegionPlots)
     buildRegion(map, plant, y, x + 1, regionData, currentRegionPlots)
@@ -34,14 +45,18 @@ function buildRegion(map, plant, y, x, regionData, currentRegionPlots = []) {
     buildRegion(map, plant, y, x - 1, regionData, currentRegionPlots)
 }
 
+let totalPrice = 0
 for (let y = 0; y < map.length; y++) {
     for (let x = 0; x < map[0].length; x++) {
-        if (observedPlots.includes(y+','+x)) {
+        if (observedPlots.includes(y + ',' + x)) {
             continue
         }
 
         const regionData = {area: 0, perimeter: 0}
         buildRegion(map, map[y][x], y, x, regionData)
-        const c = 123
+        const fencePrice = regionData.area * regionData.perimeter
+        totalPrice += fencePrice
     }
 }
+
+console.log(totalPrice)
